e932192ba65135d563cf8a49e6bcd59945dec9b8
车牛 <27235304@qq.com>
2017-08-23 11:18:00 +0800

    车牛的

--- DaShiXumMoBan/app/src/main/java/com/jiyun/qcloud/dashixummoban/ui/more/MorePageFragment.java
+++ DaShiXumMoBan/app/src/main/java/com/jiyun/qcloud/dashixummoban/ui/more/MorePageFragment.java
@@ -1,22 +1,12 @@
 package com.jiyun.qcloud.dashixummoban.ui.more;
 
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.jiyun.qcloud.dashixummoban.R;
 import com.jiyun.qcloud.dashixummoban.base.BaseFragment;
-import com.jiyun.qcloud.dashixummoban.main.GouwuActivity;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
-import butterknife.Unbinder;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -30,6 +20,7 @@ public class MorePageFragment extends BaseFragment {
 
     @Override
     protected void initData() {
+
     }
 
     @Override
@@ -41,11 +32,4 @@ public class MorePageFragment extends BaseFragment {
     public void setBundle(Bundle bundle) {
 
     }
-
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-    }
-
 }
